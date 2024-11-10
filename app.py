5377a40705fbcaf2f2963b5fb76aa8514b7d449b
unknown <you@example.com>
2024-11-10 12:12:02 +0500

    first commit

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask,request, render_template
 import numpy as np
-import pandas as pd
 import pickle
+import sklearn
+print(sklearn.__version__)
+#loading models
+dtr = pickle.load(open('models/dtr.pkl','rb'))
+preprocessor = pickle.load(open('models/preprocessor.pkl','rb'))
 
-# loading model
-model = pickle.load(open('models/model.pkl', 'rb'))
-
-# flask app
+#flask app
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
-
-
-@app.route('/predict', methods=['POST'])
+@app.route("/predict",methods=['POST'])
 def predict():
-    features = request.form['feature']
-    features = features.split(',')
-    np_features = np.asarray(features, dtype=np.float32)
+    if request.method == 'POST':
+        Year = request.form['Year']
+        average_rain_fall_mm_per_year = request.form['average_rain_fall_mm_per_year']
+        pesticides_tonnes = request.form['pesticides_tonnes']
+        avg_temp = request.form['avg_temp']
+        Area = request.form['Area']
+        Item  = request.form['Item']
 
-    # prediction
-    pred = model.predict(np_features.reshape(1, -1))
-    message = ['Cancrouse' if pred[0] == 1 else 'Not Cancrouse']
-    # print(message[0])
-    return render_template('index.html', message=message)
+        features = np.array([[Year,average_rain_fall_mm_per_year,pesticides_tonnes,avg_temp,Area,Item]],dtype=object)
+        transformed_features = preprocessor.transform(features)
+        prediction = dtr.predict(transformed_features).reshape(1,-1)
 
+        return render_template('index.html',prediction = prediction)
 
-if __name__ == '__main__':
+if __name__=="__main__":
     app.run(debug=True)
-
